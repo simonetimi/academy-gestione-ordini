436902237e1232455fb0ffbd92ce77ce269144f5
Simone <150595274+simonetimi@users.noreply.github.com>
2024-11-11 18:02:50 +0100

    add reactive form for login

--- frontend-gestione-ordini/src/app/features/auth/signup/signup.component.ts
+++ frontend-gestione-ordini/src/app/features/auth/signup/signup.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.scss'
+  styleUrl: './signup.component.scss',
 })
 export class SignupComponent {
-
+  signupForm = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.email, Validators.required]),
+    password: new FormControl('', [Validators.required]),
+  });
 }
