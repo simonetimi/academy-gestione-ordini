b6ec08d9fce1df7c0062381b78f6d48c9adaf475
Simone <150595274+simonetimi@users.noreply.github.com>
2024-11-11 12:59:50 +0100

    add DTOs

--- backend-app-gestione-ordini/src/main/java/com/lascauxacademy/backendappgestioneordini/models/OrderDTO.java
+++ backend-app-gestione-ordini/src/main/java/com/lascauxacademy/backendappgestioneordini/models/OrderDTO.java
@@ -1,4 +1,35 @@
 package com.lascauxacademy.backendappgestioneordini.models;
 
+import java.util.List;
+
 public class OrderDTO {
+    String clientId;
+
+    OrderState orderState;
+
+    List<OrderProductDTO> orderProductList;
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public OrderState getOrderState() {
+        return orderState;
+    }
+
+    public void setOrderState(OrderState orderState) {
+        this.orderState = orderState;
+    }
+
+    public List<OrderProductDTO> getOrderProductList() {
+        return orderProductList;
+    }
+
+    public void setOrderProductList(List<OrderProductDTO> orderProductList) {
+        this.orderProductList = orderProductList;
+    }
 }
