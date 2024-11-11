5bfe00205706faa6182bc814dc847e611d529914
Simone <150595274+simonetimi@users.noreply.github.com>
2024-11-11 10:04:40 +0100

    add folder structure

---
+++ backend-app-gestione-ordini/src/main/java/com/lascauxacademy/backendappgestioneordini/entities/Product.java
@@ -0,0 +1,21 @@
+package com.lascauxacademy.backendappgestioneordini.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+
+    @Column
+    double price;
+
+    @Column
+    String name;
+
+    // TODO vat enum
+
+
+}
