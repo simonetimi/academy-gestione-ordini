5434cf6324ab4dd12df314d40e59085b1931d834
Simone <150595274+simonetimi@users.noreply.github.com>
2024-11-11 09:50:01 +0100

    initial commit

---
+++ backend-app-gestione-ordini/src/main/java/com/lascauxacademy/backendappgestioneordini/BackendAppGestioneOrdiniApplication.java
@@ -0,0 +1,13 @@
+package com.lascauxacademy.backendappgestioneordini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAppGestioneOrdiniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendAppGestioneOrdiniApplication.class, args);
+    }
+
+}
---
+++ backend-app-gestione-ordini/src/test/java/com/lascauxacademy/backendappgestioneordini/BackendAppGestioneOrdiniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lascauxacademy.backendappgestioneordini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAppGestioneOrdiniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
