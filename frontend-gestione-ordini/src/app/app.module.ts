3815faceb3bfcb3bbcae3a07b19aac546f1c023c
Simone <150595274+simonetimi@users.noreply.github.com>
2024-11-12 09:43:36 +0100

    add auth button on header

--- frontend-gestione-ordini/src/app/app.module.ts
+++ frontend-gestione-ordini/src/app/app.module.ts
@@ -7,10 +7,11 @@ import { HeaderComponent } from './features/header/header.component';
 import { MainComponent } from './features/main/main.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { NgOptimizedImage } from '@angular/common';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, MainComponent],
-  imports: [BrowserModule, AppRoutingModule, NgOptimizedImage],
+  imports: [BrowserModule, AppRoutingModule, NgOptimizedImage, SharedModule],
   providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
--- frontend-gestione-ordini/src/app/shared/shared.module.ts
+++ frontend-gestione-ordini/src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from './material.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AuthButtonComponent } from './components/auth-button/auth-button.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [AuthButtonComponent],
   imports: [CommonModule, MaterialModule, ReactiveFormsModule],
-  exports: [MaterialModule, ReactiveFormsModule],
+  exports: [MaterialModule, ReactiveFormsModule, AuthButtonComponent],
 })
 export class SharedModule {}
