19e3de8f2d93d0946740a9737db7f8c1a84b23ff
tiby <cujba.tiberiu@gmail.com>
2024-11-17 14:17:20 +0100

    Revert "changed auth names for readability"
    This reverts commit c77fd8405eec4ebf24a1b6027dfd572ab5d4810b.

--- backend-app-gestione-ordini/src/main/java/com/lascauxacademy/backendappgestioneordini/services/AuthService.java
+++ backend-app-gestione-ordini/src/main/java/com/lascauxacademy/backendappgestioneordini/services/AuthService.java
@@ -4,7 +4,6 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -15,7 +14,6 @@ import org.springframework.stereotype.Service;
 import com.lascauxacademy.backendappgestioneordini.entities.Role;
 import com.lascauxacademy.backendappgestioneordini.entities.User;
 import com.lascauxacademy.backendappgestioneordini.models.ERole;
-import com.lascauxacademy.backendappgestioneordini.models.JWTAuthResponse;
 import com.lascauxacademy.backendappgestioneordini.models.LoginDto;
 import com.lascauxacademy.backendappgestioneordini.models.RegisterDto;
 import com.lascauxacademy.backendappgestioneordini.repositories.RoleRepository;
@@ -23,104 +21,95 @@ import com.lascauxacademy.backendappgestioneordini.repositories.UserRepository;
 import com.lascauxacademy.backendappgestioneordini.security.JwtTokenProvider;
 
 import jakarta.persistence.EntityExistsException;
-import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class AuthService {
-	private AuthenticationManager authenticationManager;
-	private UserRepository userRepository;
-	private RoleRepository roleRepository;
-	PasswordEncoder passwordEncoder;
-	private JwtTokenProvider jwtTokenProvider;
+    private AuthenticationManager authenticationManager;
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    PasswordEncoder passwordEncoder;
+    private JwtTokenProvider jwtTokenProvider;
 
-	public AuthService(AuthenticationManager authenticationManager, UserRepository userRepository,
-			RoleRepository roleRepository, PasswordEncoder passwordEncoder, JwtTokenProvider jwtTokenProvider) {
-		this.authenticationManager = authenticationManager;
-		this.userRepository = userRepository;
-		this.roleRepository = roleRepository;
-		this.passwordEncoder = passwordEncoder;
-		this.jwtTokenProvider = jwtTokenProvider;
-	}
+    public AuthService(AuthenticationManager authenticationManager,
+                       UserRepository userRepository,
+                       RoleRepository roleRepository,
+                       PasswordEncoder passwordEncoder,
+                       JwtTokenProvider jwtTokenProvider) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
 
-	public JWTAuthResponse login(LoginDto loginDto) throws Exception {
-		User user = userRepository.findByUsername(loginDto.getUsername())
-				.orElseThrow(() -> new EntityNotFoundException("User doesnìt exists!"));
+    public String login(LoginDto loginDto) {
 
-		// create an authentication token using the provided username and password
-		Authentication authentication = authenticationManager
-				.authenticate(new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword()));
+        // create an authentication token using the provided username and password
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword()));
 
-		// set the authentication in the security context
-		SecurityContextHolder.getContext().setAuthentication(authentication);
+        // set the authentication in the security context
+        SecurityContextHolder.getContext().setAuthentication(authentication);
 
-		// generate a JWT token for the authenticated user
-		String token = jwtTokenProvider.generateToken(authentication);
-		
-		// create an instance of the response, jwtAuthResponse
-		JWTAuthResponse jwtAuthResponse = new JWTAuthResponse();
-		
-		//get the role from the user
-		String role = this.userRole(loginDto.getUsername());
+        // generate a JWT token for the authenticated user
+        String token = jwtTokenProvider.generateToken(authentication);
+        return token;
+    }
 
-		// set the information for the response
-		jwtAuthResponse.setUsername(user.getUsername());
-		jwtAuthResponse.setToken(token);
-		jwtAuthResponse.setEmail(user.getEmail());
-		jwtAuthResponse.setId(user.getId());
-		jwtAuthResponse.setRole(role);
-		jwtAuthResponse.setTokenExpireDate(System.currentTimeMillis() + 1000 * 60 * 60 * 10); // 10 hours in millis
+    public String register(RegisterDto registerDto) throws Exception {
 
-		//return it
-		return jwtAuthResponse;
-	}
+        if (userRepository.existsByUsername(registerDto.getUsername())) {
+            throw new EntityExistsException("username_exists");
+        }
 
-	public String register(RegisterDto registerDto) throws Exception {
+        if (userRepository.existsByEmail(registerDto.getEmail())) {
+            throw new EntityExistsException("email_exists");
+        }
 
-		if (userRepository.existsByUsername(registerDto.getUsername())) {
-			throw new EntityExistsException("username_exists");
-		}
+        User user = new User();
+        user.setUsername(registerDto.getUsername());
+        user.setEmail(registerDto.getEmail());
 
-		if (userRepository.existsByEmail(registerDto.getEmail())) {
-			throw new EntityExistsException("email_exists");
-		}
+        // encode the user's password before saving
+        user.setPassword(passwordEncoder.encode(registerDto.getPassword()));
 
-		User user = new User();
-		user.setUsername(registerDto.getUsername());
-		user.setEmail(registerDto.getEmail());
+        Set<Role> roles = new HashSet<>();
 
-		// encode the user's password before saving
-		user.setPassword(passwordEncoder.encode(registerDto.getPassword()));
+        // if roles are provided in the registration data, find and add them to the user's roles
+        if (registerDto.getRoles() != null && registerDto.getRoles().contains("admin")) {
+            Role userRole = roleRepository.findByRoleName(ERole.ROLE_ADMIN).get();
+            roles.add(userRole);
+        } else {
+            Role userRole = roleRepository.findByRoleName(ERole.ROLE_OPERATOR).get();
+            roles.add(userRole);
+        }
 
-		Set<Role> roles = new HashSet<>();
+        user.setRoles(roles);
+        userRepository.save(user);
 
-		// if roles are provided in the registration data, find and add them to the
-		// user's roles
-		if (registerDto.getRoles() != null && registerDto.getRoles().contains("admin")) {
-			Role userRole = roleRepository.findByRoleName(ERole.ROLE_ADMIN).get();
-			roles.add(userRole);
-		} else {
-			Role userRole = roleRepository.findByRoleName(ERole.ROLE_OPERATOR).get();
-			roles.add(userRole);
-		}
+        return "User registered successfully!.";
+    }
 
-		user.setRoles(roles);
-		userRepository.save(user);
+    // helper method to convert a role string to the corresponding enum type
+    public ERole getRole(String role) {
+        if (role.equals("ADMIN"))
+            return ERole.ROLE_ADMIN;
+        else
+            return ERole.ROLE_OPERATOR;
+    }
 
-		return "User registered successfully!.";
-	}
+    // method to retrieve the role of a user based on their username
+    public String userRole(String username) throws Exception {
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new Exception("User not found"));
 
-	// method to retrieve the role of a user based on their username
-	public String userRole(String username) throws Exception {
-		User user = userRepository.findByUsername(username).orElseThrow(() -> new Exception("User not found"));
+        Set<Role> roles = user.getRoles();
+        if (roles.isEmpty()) {
+            throw new Exception("No roles found for the user");
+        }
 
-		Set<Role> roles = user.getRoles();
-		if (roles.isEmpty()) {
-			throw new Exception("No roles found for the user");
-		}
-
-		// return the name of the first role in the user's role set as a string
-		Role role = roles.iterator().next();
-		return role.getRoleName().name();
-	}
+        // return the name of the first role in the user's role set as a string
+        Role role = roles.iterator().next();
+        return role.getRoleName().name();
+    }
 
 }
