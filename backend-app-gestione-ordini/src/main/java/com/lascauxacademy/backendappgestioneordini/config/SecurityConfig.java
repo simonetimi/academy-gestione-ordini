e1bbbd52b1560ae1a8d9d689a13000df32791610
Simone <150595274+simonetimi@users.noreply.github.com>
2024-11-17 17:45:41 +0100

    move cors config before other requestMatchers and change permitAll to authenticated for protected routes

--- backend-app-gestione-ordini/src/main/java/com/lascauxacademy/backendappgestioneordini/config/SecurityConfig.java
+++ backend-app-gestione-ordini/src/main/java/com/lascauxacademy/backendappgestioneordini/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.lascauxacademy.backendappgestioneordini.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -35,41 +36,42 @@ public class SecurityConfig {
     }
 
     // bean that returns a password encoder used to securely store passwords
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     // bean that provides the AuthenticationManager for the app
-	// AuthenticationManager is needed for handling authentication in Spring Security
-	@Bean
-	public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
-		return authConfig.getAuthenticationManager();
-	}
-	
-	 @Bean
-	    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    // AuthenticationManager is needed for handling authentication in Spring Security
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
 
-	    	http.csrf(csrf -> csrf.disable()) //Cross-Site Request Forgery is disbled beacuse using JWT
-	        .authorizeHttpRequests((authorize) -> authorize
-	        		.requestMatchers("/auth/**").permitAll()
-	                .requestMatchers("/clients", "/clients/**").permitAll()
-	                .requestMatchers("/orders", "/orders/**").permitAll()
-	                .requestMatchers("/products", "/products/**").permitAll())
-	        // handle exceptions during authentication and provide an entry point for unauthorized access
-	        .exceptionHandling( exception -> exception
-	                .authenticationEntryPoint(authenticationEntryPoint)// set entry point for authentication failures
-	                // configure session management, using stateless sessions as we're using JWT 
-	        		).sessionManagement( session -> session
-	                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)// stateless means no session is stored
-	        );
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-	        // add custom JWT filter before the default username/password filter in the security chain , 
-	    	//check token before authentication
-	    	http.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
-	    	
-	    	// build and return the configured security filter chain
-	    	return http.build();
-	    }
+        http.csrf(csrf -> csrf.disable()) //Cross-Site Request Forgery is disbled beacuse using JWT
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/clients", "/clients/**").authenticated()
+                        .requestMatchers("/orders", "/orders/**").authenticated()
+                        .requestMatchers("/products", "/products/**").authenticated())
+                // handle exceptions during authentication and provide an entry point for unauthorized access
+                .exceptionHandling(exception -> exception
+                                .authenticationEntryPoint(authenticationEntryPoint)// set entry point for authentication failures
+                        // configure session management, using stateless sessions as we're using JWT
+                ).sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)// stateless means no session is stored
+                );
+
+        // add custom JWT filter before the default username/password filter in the security chain ,
+        //check token before authentication
+        http.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        // build and return the configured security filter chain
+        return http.build();
+    }
 
 }
--- backend-app-gestione-ordini/src/main/java/com/lascauxacademy/backendappgestioneordini/config/WebConfig.java
+++ backend-app-gestione-ordini/src/main/java/com/lascauxacademy/backendappgestioneordini/config/WebConfig.java
@@ -14,6 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
